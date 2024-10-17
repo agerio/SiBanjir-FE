f6fb6ddeedb28559caba74d9db7b6db7dc28a13e
Jovian <ajovian440@gmail.com>
2024-10-17 16:07:43 +1000

    Combine add friend pages into one singile page

--- app/group_related/add_friend.tsx
+++ app/group_related/add_friend.tsx
@@ -1,28 +1,103 @@
 import React, { useState } from 'react';
-import { SafeAreaView, TextInput, Text, TouchableOpacity, StyleSheet, View } from 'react-native';
-import { useRouter } from 'expo-router';
+import { SafeAreaView, TextInput, Text, TouchableOpacity, StyleSheet, View, ActivityIndicator, Image, Alert } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import axios from 'axios';
+import { API_URL } from "@/context/GlobalContext";
 
 export default function AddFriendSearch() {
     const [searchQuery, setSearchQuery] = useState('');
-    const router = useRouter();
+    const [isLoadingSearch, setIsLoadingSearch] = useState(false);
+    const [isLoadingAdd, setIsLoadingAdd] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    const [userData, setUserData] = useState(null);
+
+    const handleSearch = async () => {
+        if (!searchQuery) {
+            setError('Please enter a username to search.');
+            return;
+        }
+
+        setIsLoadingSearch(true);
+        setError(null);
+        setUserData(null);
+
+        try {
+            const response = await axios.post(`${API_URL}/user/searchFriend`, {
+                recipient_username: searchQuery
+            });
+
+            if (response.data && response.data.username) {
+                setUserData(response.data);
+            }
+        } catch (error) {
+            setError('User not found.');
+        } finally {
+            setIsLoadingSearch(false);
+        }
+    };
+
+    const handleAddFriend = async () => {
+        setIsLoadingAdd(true);
+        try {
+            const response = await axios.post(`${API_URL}/user/sendInvitation`, { 
+                recipient_username: userData.username
+            });
+            if (response.data.message === "Invitation sent successfully") {
+                Alert.alert("Success", "Successfully sent a friend request! Please wait for their response.");
+            }
+        } catch (error) {
+            Alert.alert("Fail", "An error occurred. Please try again.");
+        } finally {
+            setIsLoadingAdd(false);
+        }
+    };
 
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
-                <Text style={styles.header}>Adding New Friend</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Enter username"
-                    value={searchQuery}
-                    onChangeText={setSearchQuery}
-                    placeholderTextColor="#999"
-                />
-                <TouchableOpacity
-                    style={styles.searchButton}
-                    onPress={() => router.push({ pathname: '../group_related/AddFriendResult', params: { searchQuery } })}
-                >
-                    <Text style={styles.buttonText}>Search</Text>
-                </TouchableOpacity>
+                <Text style={styles.header}>Add New Friend</Text>
+                <View style={styles.searchContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Enter username"
+                        value={searchQuery}
+                        onChangeText={setSearchQuery}
+                        placeholderTextColor="#999"
+                    />
+                    <TouchableOpacity
+                        style={styles.searchButton}
+                        onPress={handleSearch}
+                        disabled={isLoadingSearch}
+                    >
+                        {isLoadingSearch ? (
+                            <ActivityIndicator size={24} color="#fff" />
+                        ) : (
+                            <Ionicons name="search" size={24} color="white" />
+                        )}
+                    </TouchableOpacity>
+                </View>
+                {error && <Text style={styles.errorText}>{error}</Text>}
+
+                {userData && (
+                    <View style={styles.profileContainer}>
+                        <Image 
+                            source={userData.profile_picture ? { uri: userData.profile_picture } : require('../../assets/images/user-icon.png')} 
+                            style={styles.profileImage}
+                        />
+                        <Text style={styles.friendName}>{userData.username}</Text>
+                        <TouchableOpacity 
+                            style={styles.addButton} 
+                            onPress={handleAddFriend}
+                            disabled={isLoadingAdd}
+                        >
+                            {isLoadingAdd ? (
+                                <ActivityIndicator size="small" color="#fff" />
+                            ) : (
+                                <Text style={styles.buttonText}>Add User</Text>
+                            )}
+                        </TouchableOpacity>
+                    </View>
+                )}
             </View>
         </SafeAreaView>
     );
@@ -44,27 +119,75 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: 'bold',
         color: '#fff',
-        marginBottom: 30,
+        marginBottom: 20,
+    },
+    searchContainer: {
+        width: '100%',
+        flexDirection: 'row',
+        padding: 10,
     },
     input: {
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 10,
-        padding: 10,
-        fontSize: 16,
-        backgroundColor: '#fff',
-        marginBottom: 15,
-        width: '85%', // Reduced width to create space on sides
-        color: '#000', // Ensure text is visible
+        flex: 1,
+        height: 50,
+        backgroundColor: '#333',
+        borderRadius: 8,
+        paddingHorizontal: 10,
+        color: '#fff',
     },
     searchButton: {
-        backgroundColor: '#444',
-        paddingVertical: 15,
-        paddingHorizontal: 30,
-        borderRadius: 10,
-        width: '35%', // Match input width
         alignItems: 'center',
+        justifyContent: "center",
+        paddingVertical: 8,
+        paddingHorizontal: 12,
+        borderRadius: 8,
+        marginLeft: 8,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+        elevation: 10,
+        backgroundColor: '#46468a',
+    },
+    errorText: {
+        color: 'red',
+        fontSize: 16,
         marginTop: 10,
+    },
+    profileContainer: {
+        alignItems: 'center',
+        marginTop: 30,
+        padding: 15,
+        borderRadius: 10,
+        width: '100%',
+    },
+    profileImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        marginBottom: 10,
+    },
+    friendName: {
+        fontSize: 20,
+        color: '#fff',
+        marginBottom: 10,
+    },
+    addButton: {
+        backgroundColor: '#46468a',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 10,
+        marginTop: 7,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+        elevation: 10,
     },
     buttonText: {
         color: '#fff',
